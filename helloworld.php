44ff0d7079b3666224e67234ce588cb355eb516a
johndmulhausen <johndylan@google.com>
2014-05-09 18:58:22 -0700

    Initial commit

---
+++ helloworld.php
@@ -0,0 +1,17 @@
+<html>
+  <body>
+    <?php
+	// [START formprocessing]
+    if (array_key_exists('content', $_POST)) {
+      echo "You wrote:<pre>\n";
+      echo htmlspecialchars($_POST['content']);
+      echo "\n</pre>";
+    }
+	// [END formprocessing]
+    ?>
+    <form action="/sign" method="post">
+      <div><textarea name="content" rows="3" cols="60"></textarea></div>
+      <div><input type="submit" value="Sign Guestbook"></div>
+    </form>
+  </body>
+</html>
